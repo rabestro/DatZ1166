1ef71df4a115c7d76a9e4abdd72a02a463a0ca6f
Jegors Cemisovs <jegors.cemisovs@outlook.lv>
2021-09-15 16:49:28 +0300

    Add comments

--- H10/main.cpp
+++ H10/main.cpp
@@ -4,17 +4,33 @@
 #include <fstream>
 
 using namespace std;
-
+/**
+ * Lab. H10
+ *
+ * Read text from two given text files.
+ * Text in both given file contains identified words.
+ * One line in file is one word in format <integer ID> <Word max 30 characters>
+ *
+ * Merge both files in third file.
+ * Print data in third file in descending order by ID value.
+ * The same ID may not appear twice in third file.
+ * If two words have the same ID, concatenate them in third file.
+ */
 int main() {
     map<int, string> map;
     int id;
+    string word;
     ifstream in;
+
     in.open("one.txt");
-    for (string word; in >> id >> word;) map[id] = word;
+    while (in >> id >> word) map[id] = word;
     in.close();
+
     in.open("two.txt");
-    for (string word; in >> id >> word;) map[id] += word;
+    while (in >> id >> word) map[id] += word;
+    in.close();
 
     ofstream out("out.txt");
     for (auto& i : map) out << i.first << " " << i.second << endl;
+    out.close();
 }
