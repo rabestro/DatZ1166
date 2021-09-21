2e1973b45416c13f66d910f61a0580860c0bd2e5
Jegors Cemisovs <jegors.cemisovs@outlook.lv>
2021-09-21 20:15:56 +0300

    Update Java solution

--- F18/java/src/main/java/lv/id/jc/f18/Main.java
+++ F18/java/src/main/java/lv/id/jc/f18/Main.java
@@ -4,17 +4,21 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
-public class Main {
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+public final class Main {
     public static void main(String[] args) throws IOException {
-        var map = new Scanner(Paths.get("in.txt"))
-                .useDelimiter("\\W+")
-                .tokens()
-                .map(String::length)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-        Files.writeString(Paths.get("out.txt"), map.toString());
+        Files.writeString(Paths.get("out.txt"),
+                new Scanner(Paths.get("in.txt"))
+                        .useDelimiter("\\W+")
+                        .tokens()
+                        .map(String::length)
+                        .collect(groupingBy(identity(), counting()))
+                        .toString()
+        );
     }
 }
