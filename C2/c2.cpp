269f02465b90db58b2be0306b3d5ed060485dc12
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2021-10-23 13:59:14 +0300

    Add c2

---
+++ C2/c2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+struct LinkedList {
+    int value;
+    LinkedList *next;
+};
+
+void deleteFirstTwoDescending(LinkedList **first) {
+    auto root = *first;
+    auto prev = root;
+    for (auto curr = prev->next; curr; root = prev, prev = curr, curr = curr->next) {
+        if (prev->value > curr->value) {
+            root->next = curr->next;
+            return;
+        }
+    }
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    LinkedList a, b, c;
+    c.value = 6; c.next = NULL;
+    b.value = 7; b.next = &c;
+    a.value = 3; a.next = &b;
+
+    auto root = &a;
+    cout << a.value << endl << a.next << endl;
+    deleteFirstTwoDescending(&root);
+
+    cout << a.value << endl << a.next << endl;
+
+    return 0;
+}
