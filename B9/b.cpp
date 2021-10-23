924398bb77ed9e76d9043b44ca4810a6fcc16e93
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2021-10-23 12:52:59 +0300

    Add comment to the task

--- B9/b.cpp
+++ B9/b.cpp
@@ -2,7 +2,19 @@
 #include <fstream>
 
 using namespace std;
-
+/*
+ * B[9]. Given text file.
+ *
+ * Write program in C++ to process the file line by line.
+ * It’s allowed to use C++-style character strings.
+ * Function, which calculates number of the line (numbering starts with 0)
+ * with the biggest amount of digits in it.
+ *
+ * Example (with // to denote newline).
+ * File: “A123B4.//56c789-0//12x345”,
+ * answer 1.
+ * Comment. Amounts of digits per line: 4,6,5.
+ */
 int lineNumWithMostDigits(string filename) {
     ifstream file(filename);
     string line;
