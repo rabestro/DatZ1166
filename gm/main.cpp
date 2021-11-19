72d922b31891fdcb3105e3847760ed2340b0339f
Jegors Cemisovs <jegors.cemisovs@accenture.com>
2021-11-19 10:47:18 +0200

    Add lab gm

---
+++ gm/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+
+using namespace std;
+
+void print(list<int> list) {
+    for (int n: list) cout << n << " ";
+    cout << endl;
+}
+
+void removeLower(list<int> &list) {
+    if (list.size() < 2) return;
+
+    auto it = list.begin();
+    int prev = *it;
+    for (auto i = ++it; i != list.end(); i++) {
+//        cout << "p=" << prev << " i=" << *i << endl;
+        if (*i < prev) {
+            list.erase(i);
+        } else {
+            prev = *i;
+        }
+    }
+
+}
+
+int main() {
+    // read from file
+    ifstream in("in.txt");
+
+    list<int> myList;
+
+    while (!in.eof()) {
+        int n;
+        in >> n;
+        myList.push_back(n);
+    }
+    in.close();
+
+    print(myList);
+    removeLower(myList);
+    print(myList);
+    return 0;
+}
